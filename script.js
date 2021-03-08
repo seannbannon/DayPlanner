1aed80219469fac4c3d807f3de5c5489eb14cdd1
Sean Bannon <seannbannon@gmail.com>
2021-03-08 14:56:01 -0600

    added time blocks

---
+++ script.js
@@ -0,0 +1,9 @@
+// display current day on top of page
+
+// below is a box of business hours
+
+// time blocks change color based on if past, present, or future
+
+// when clicking time block, you can enter an event and save it in local storage
+
+// when refreshed page, items stay
